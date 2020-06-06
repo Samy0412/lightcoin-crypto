85c3ddb905a79ea0ddb11d296d6ec8eb603dbc0a
Samy0412 <samantha.gadet@gmail.com>
2020-06-06 19:33:13 +0000

    final version

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-let balance = 500.00;
+// let balance = 500.0;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    // Have the account balance start at $0 since that makes more sense.
+    // this.balance = 0;
+  }
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      return false;
+    }
   }
-
+}
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return this.account.balance - this.amount >= 0;
+  }
 }
 
-
-
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
+console.log("Starting Balance:", myAccount.balance);
 
-t1 = new Withdrawal(50.25);
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+console.log(t1);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+//console.log(myAccount.balance);
+const t3 = new Withdrawal(80.0, myAccount);
+t3.commit();
+console.log(t3);
+//console.log(myAccount.balance);
+console.log("Ending Balance:", myAccount.balance);
+console.log(myAccount.transactions);
